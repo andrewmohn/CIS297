1d0d76ae126454d116f2d900287b889538420a40
andrewmohn <amohn@umich.edu>
2017-02-21 17:44:05 -0500

    Feb 21 5:45

---
+++ bettingSimulator/bettingSimulator/bettingSimulator/gameTemplate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bettingSimulator
+{
+    class gameTemplate<T> : IComparable<T>
+    {
+        public int CompareTo(T value)
+        {
+            
+            return 
+        }
+    }
+}
